8da586401853d91e0adbcae4bffa1e0496243a92
Larisio <multilirs@gmail.com>
2017-08-01 23:43:52 +0200

    Change MenuSystem

--- CTermu/MenuSystem.cs
+++ CTermu/MenuSystem.cs
@@ -9,7 +9,8 @@ namespace CTermu
     class MenuSystem
     {
         public string Question { get; set; }
-        public string Options { get; set; }
+        public string[] Options { get; set; }
+        public string[] Options = 
         public int MaxWidth { get; set; }
         public int MaxHeight { get; set; }
     }
