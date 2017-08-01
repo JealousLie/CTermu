453ed8aca27d3ada8b26ac2a639c1b224340d5e6
Larisio <multilirs@gmail.com>
2017-08-01 23:34:53 +0200

    Add MenuSystem

---
+++ CTermu/MenuSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTermu
+{
+    class MenuSystem
+    {
+        public string Question { get; set; }
+        public string Options { get; set; }
+        public int MaxWidth { get; set; }
+        public int MaxHeight { get; set; }
+    }
+}
