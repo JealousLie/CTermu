379273752a08f54724633b49dd314ab7ee585ac4
Larisio <multilirs@gmail.com>
2017-08-01 23:45:35 +0200

    command MenuSystem

--- CTermu/MenuSystem.cs
+++ CTermu/MenuSystem.cs
@@ -9,8 +9,7 @@ namespace CTermu
     class MenuSystem
     {
         public string Question { get; set; }
-        public string[] Options { get; set; }
-        public string[] Options = 
+        /*public string[] Options { get; set; }*/ public string[] Options = 
         public int MaxWidth { get; set; }
         public int MaxHeight { get; set; }
     }
