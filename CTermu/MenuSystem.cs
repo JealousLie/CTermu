46545395b7a17920a62c816b8f960af01671a374
Larisio <multilirs@gmail.com>
2017-08-15 19:40:22 +0200

    Test des ersten Menüaufbaus //Kommentar in MenuSystem

--- CTermu/MenuSystem.cs
+++ CTermu/MenuSystem.cs
@@ -8,9 +8,48 @@ namespace CTermu
 {
     class MenuSystem
     {
-        public string Question { get; set; }
+        /*
+        public string Question { get; set; } = { "Bildskalierung ändern?" };
         public string[] Options { get; set; } = { "yes", "no" };
         public int MaxWidth { get; set; }
         public int MaxHeight { get; set; }
+        
+
+        /////////////////////////////////////
+        // Test Aufbau                     //
+        // Verbesserungswürdig :)          //
+        /////////////////////////////////////
+
+        static void Main(string[] args)
+        {
+            string Question = "Bildskalierung ändern?";
+            string[] Options = { "yes", "no", "Bestimmt größer als die frage, alles andere ist unlogisch", "nicht größer" };
+            int menuWidth = (Question.Length);
+
+            for (int i = 0; i != Options.Length; i++)
+            {
+
+                string temp = Options[i];
+
+                if (menuWidth < temp.Length)
+                {
+
+                    menuWidth = temp.Length;
+
+                }
+
+            }
+            Console.WriteLine(menuWidth);
+            string line = new string('-', menuWidth);
+            string space = new string(' ', menuWidth - Question.Length);
+            Console.WriteLine(space);
+            Console.WriteLine( "{0}\n|{1}{3}|\n{0}", line, Question, space);
+            Console.ReadKey();
+        }   
+        */
+
+
+
+
     }
 }
